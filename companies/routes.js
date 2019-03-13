43a2b172cc08ab5ca70ae15a45742cb81949c527
Teeeep <casperjantepe@gmail.com>
2019-03-13 11:53:04 +0100

    fixed companies endpoint

--- companies/routes.js
+++ companies/routes.js
@@ -3,7 +3,7 @@ const Company = require('./model')
 
 const router = new Router()
 
-router.get('./companies', (req, res, next) => {
+router.get('/companies', (req, res, next) => {
     Company
         .findAll()
         .then(companies => {
@@ -12,7 +12,7 @@ router.get('./companies', (req, res, next) => {
         .catch(error => next(error))
 })
 
-router.get('./companies/:id', (req, res, next) => {
+router.get('/companies/:id', (req, res, next) => {
     Company
         .findById(req.params.id)
         .then(company => {
@@ -46,10 +46,29 @@ router.put('/companies/:id', (req, res, next) => {
         .then(company => {
             if (!company) {
                 return res.status(404).send({
-                    message: `Customer does not exist`
+                    message: `Company does not exist`
                 })
             }
             return company.update(req.body).then(company => res.send(company))
         })
         .catch(error => next(error))
 })
+
+router.delete('/companies/:id', (req, res, next) => {
+    Company
+        .findById(req.params.id)
+        .then(company => {
+            if (!company) {
+                return res.status(404).send({
+                    message: `Company does not exist`
+                })
+            }
+            return company.destroy()
+                .then(() => res.send({
+                    message: `Company was deleted`
+                }))
+        })
+        .catch(error => next(error))
+})
+
+module.exports = router
